5f5e459c83faf7a6a809575659dd658e2e12786e
Ruchi <ruchisaini.0404@gmail.com>
2021-11-11 15:04:09 +1100

    added information in schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,38 @@
+INSERT INTO department(name)
+VALUES 
+("IT"),
+("Finance & Accounting"),
+("Sales & Marketing"),
+("Operations"),
+("Human Resources");
+
+INSERT INTO role(title, salary, department_id)
+VALUES
+('Full Stack Developer', 90000, 1),
+('Software Engineer', 120000, 1),
+('Accountant', 75000, 2),
+('Financial Analyst', 80000, 2),
+('Sales Person', 85000, 3),
+('Marketing Manager', 180000, 3),
+('Project Manager', 120000, 4),
+('Operations Manager', 70000, 4),
+('HR Director', 90000, 5),
+('Recruitment Officer', 80000, 5);
+
+-- employee seeds
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
+('Mabel', 'Miller', 1 , null),
+('John', 'Gomez', 2, 3),
+('William', 'Toz', 3, null),
+('Ana', 'West', 4, 2),
+('Katherine', 'Jacobs', 6, null),
+('Fay', 'Beneventi', 8, 4),
+('David', 'Dhawan', 5, null),
+('Varun', 'Dhawan', 7, null),
+('Kerri', 'Allen', 9, 5),
+('Samiksha', 'Green', 10, null);
+    
+
+
+
